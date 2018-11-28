7f5463e9394d49703312983db9a2cf3838b813b9
fan <652810758@qq.com>
2018-11-28 20:27:40 +0800

    修改redis配置路径

--- src/main/java/com/xch/study/redis/controller/RedisController.java
+++ src/main/java/com/xch/study/redis/controller/RedisController.java
@@ -4,14 +4,12 @@ import com.xch.study.redis.entity.UsereEntity;
 import com.xch.study.redis.service.RedisCommonService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author fgs
@@ -24,6 +22,28 @@ public class RedisController {
 
     @Autowired
     private RedisCommonService redisCommonService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    @RequestMapping(value = "/setSetValue", method = RequestMethod.POST)
+    @ApiOperation(value = "出入string类型的数值", notes = "redis操作", httpMethod = "POST", response = Map.class)
+    public void setSetValue() {
+        Set<String> set1 = new HashSet<>();
+        set1.add("set1");
+        set1.add("set2");
+        set1.add("set3");
+        redisTemplate.opsForSet().add("set1", set1);
+        Set<String> resultSet = redisTemplate.opsForSet().members("set1");
+        Iterator<String> iterator = resultSet.iterator();
+//        Iterator<String> iterator = set1.iterator();
+        while (iterator.hasNext()){
+            String entity=iterator.next();
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>:" + entity);
+        }
+
+    }
 
     @RequestMapping(value = "/setStringValue", method = RequestMethod.POST)
     @ApiOperation(value = "出入string类型的数值", notes = "redis操作", httpMethod = "POST", response = Map.class)
@@ -57,18 +77,36 @@ public class RedisController {
     @RequestMapping(value = "/setMapValue", method = RequestMethod.POST)
     @ApiOperation(value = "出入List类型的数值", notes = "redis操作", httpMethod = "POST", response = Map.class)
     public void setMapValue() {
-        Map<String,UsereEntity> map = new HashMap<>();
+        Map<String, UsereEntity> map = new HashMap<>();
         UsereEntity entity;
         for (int i = 0; i < 10; i++) {
             entity = new UsereEntity();
             entity.setName("name" + i);
             entity.setSex("sex" + i);
-            map.put("key"+i,entity);
+            map.put("key" + i, entity);
         }
         redisCommonService.delete("m1");
 
-        redisCommonService.saveMap("m1",map);
-        System.out.println(">>>>>>>>>>>>>>>>>>>"+redisCommonService.queryMap("m1","key1"));
+        redisCommonService.saveMap("m1", map);
+        System.out.println(">>>>>>>>>>>>>>>>>>>" + redisCommonService.queryMap("m1", "key1"));
     }
 
+    @RequestMapping(value = "/setMapListValue", method = RequestMethod.POST)
+    @ApiOperation(value = "出入List类型的数值", notes = "redis操作", httpMethod = "POST", response = Map.class)
+    public void setMapListValue() {
+        Map<String, List<UsereEntity>> map = new HashMap<>();
+        UsereEntity entity;
+        List<UsereEntity> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            entity = new UsereEntity();
+            entity.setName("name" + i);
+            entity.setSex("sex" + i);
+            list.add(entity);
+        }
+        map.put("mapList", list);
+        redisCommonService.delete("ml1");
+
+        redisCommonService.saveMap("ml1", map);
+        System.out.println(">>>>>>>>>>>>>>>>>>>" + redisCommonService.queryMap("ml1", "mapList"));
+    }
 }
