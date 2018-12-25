3952d900ab2fc340740e88eaef430c5762904fa1
fan <652810758@qq.com>
2018-12-25 16:29:40 +0800

    websocket代码更新

--- src/main/java/com/xch/study/websocket/SocketController.java
+++ src/main/java/com/xch/study/websocket/SocketController.java
@@ -1,15 +1,13 @@
 package com.xch.study.websocket;
 
-import com.xch.study.utils.IpUtils;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Map;
 
 /**
@@ -21,28 +19,28 @@ import java.util.Map;
 public class SocketController {
 
     /**
-     * view:(跳转到JSP界面).
-     *
-     * @param map
+     * view:(跳转到JSP界面)
+     * @param name
+     * @param model
      * @return
      */
-    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
-    public String view(Map<String, Object> map) {
-        map.put("name", "SpringBoot");
-        map.put("date", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+    @RequestMapping(value = {"/{name}"}, method = RequestMethod.GET)
+    public String view(@PathVariable String name, Model model) {
+        //通过Model进行对象数据的传递
+        model.addAttribute("username", name);
         return "index";
     }
 
     @RequestMapping(value = "/sendMessage", method = RequestMethod.POST)
     @ApiOperation(value = "消息发送", notes = "消息发送", httpMethod = "POST", response = Map.class)
     public void sendMessage(HttpServletRequest request) {
-        try {
-            MessageEntity bean = new MessageEntity();
-            bean.setInfoSourceIP(IpUtils.getIpAddr(request));
-            WebSocket.sendInfo(bean);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            MessageEntity bean = new MessageEntity();
+//            bean.setInfoSourceIP(IpUtils.getIpAddr(request));
+//            WebSocket.sendInfo(bean);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
--- src/main/java/com/xch/study/websocket/WebSocket.java
+++ src/main/java/com/xch/study/websocket/WebSocket.java
@@ -1,121 +1,157 @@
 package com.xch.study.websocket;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-/**
- * @author fgs
- * @data 2018/12/25 9:53
- */
-@ServerEndpoint(value = "/websocket", encoders = {ServerEncoder.class})
 @Component
+@ServerEndpoint(value = "/websocket/{username}", encoders = {ServerEncoder.class})
 public class WebSocket {
-    //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
-    private static int onlineCount = 0;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
     private static CopyOnWriteArraySet<WebSocket> webSocketSet = new CopyOnWriteArraySet<>();
-
-    //与某个客户端的连接会话，需要通过它来给客户端发送数据
+    /**
+     * 在线人数
+     */
+    public static int onlineNumber = 0;
+    /**
+     * 以用户的姓名为key，WebSocket为对象保存起来
+     */
+    private static Map<String, WebSocket> clients = new ConcurrentHashMap<>();
+    /**
+     * 会话
+     */
     private Session session;
+    /**
+     * 用户名称
+     */
+    private String username;
 
     /**
-     * 连接建立成功调用的方法
+     * 建立连接
+     *
+     * @param session
      */
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(@PathParam("username") String username, Session session) {
+//        webSocketSet.add(this);
+        onlineNumber++;
+        logger.info("现在来连接的客户id：" + session.getId() + "用户名：" + username);
+        this.username = username;
         this.session = session;
-        webSocketSet.add(this);     //加入set中
-        addOnlineCount();           //在线数加1
-        System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
+        logger.info("有新连接加入！ 当前在线人数" + onlineNumber);
         try {
-            MessageEntity bean = new MessageEntity();
-            bean.setMsgContent("websocket已连接");
-            sendMessage(bean);
+            //messageType 1代表上线 2代表下线 3代表在线名单 4代表普通消息
+            //先给所有人发送通知，说我上线了
+            Map<String, Object> map1 = new HashMap<>();
+            map1.put("messageType", 1);
+            map1.put("username", username);
+            sendMessageAll(JSON.toJSONString(map1), username);
+
+            //把自己的信息加入到map当中去
+            clients.put(username, this);
+            //给自己发一条消息：告诉自己现在都有谁在线
+            Map<String, Object> map2 = new HashMap<>();
+            map2.put("messageType", 3);
+            //移除掉自己
+            Set<String> set = clients.keySet();
+            map2.put("onlineUsers", set);
+            sendMessageTo(JSON.toJSONString(map2), username);
         } catch (IOException e) {
-            System.out.println("IO异常");
+            logger.info(username + "上线的时候通知所有人发生了错误");
         }
     }
 
+    @OnError
+    public void onError(Session session, Throwable error) {
+        logger.info("服务端发生了错误" + error.getMessage());
+    }
+
     /**
-     * 连接关闭调用的方法
+     * 连接关闭
      */
     @OnClose
     public void onClose() {
-        webSocketSet.remove(this);  //从set中删除
-        subOnlineCount();           //在线数减1
-        System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
+//        webSocketSet.remove(this);
+        onlineNumber--;
+        clients.remove(username);
+        try {
+            //messageType 1代表上线 2代表下线 3代表在线名单  4代表普通消息
+            Map<String, Object> map1 = new HashMap<>();
+            map1.put("messageType", 2);
+            map1.put("onlineUsers", clients.keySet());
+            map1.put("username", username);
+            sendMessageAll(JSON.toJSONString(map1), username);
+        } catch (IOException e) {
+            logger.info(username + "下线的时候通知所有人发生了错误");
+        }
+        logger.info("有连接关闭！ 当前在线人数" + onlineNumber);
     }
 
     /**
-     * 收到客户端消息后调用的方法
+     * 收到客户端的消息
      *
-     * @param message 客户端发送过来的消息
+     * @param message 消息
+     * @param session 会话
      */
     @OnMessage
     public void onMessage(String message, Session session) {
-        System.out.println("来自客户端的消息:" + message);
-        MessageEntity bean = null;
-        //群发消息
-        for (WebSocket item : webSocketSet) {
-            try {
-                bean = new MessageEntity();
-                bean.setMsgContent(message);
-                item.sendMessage(bean);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    /**
-     * 发生错误时调用
-     *
-     * @param session
-     * @param error
-     */
-    @OnError
-    public void onError(Session session, Throwable error) {
-        System.out.println("发生错误");
-        error.printStackTrace();
-    }
-
-
-    public void sendMessage(Object message) throws IOException {
         try {
-            this.session.getBasicRemote().sendObject(message);
-        } catch (EncodeException e) {
-            e.printStackTrace();
+            logger.info("来自客户端消息：" + message + "客户端的id是：" + session.getId());
+            JSONObject jsonObject = JSON.parseObject(message);
+            String textMessage = jsonObject.getString("message");
+            String fromusername = jsonObject.getString("username");
+            String tousername = jsonObject.getString("to");
+            //如果不是发给所有，那么就发给某一个人
+            //messageType 1代表上线 2代表下线 3代表在线名单  4代表普通消息
+            Map<String, Object> map1 = new HashMap<>();
+            map1.put("messageType", 4);
+            map1.put("textMessage", textMessage);
+            map1.put("fromusername", fromusername);
+            if (tousername.equals("All")) {
+                map1.put("tousername", "所有人");
+                sendMessageAll(JSON.toJSONString(map1), fromusername);
+            } else {
+                map1.put("tousername", tousername);
+                sendMessageTo(JSON.toJSONString(map1), tousername);
+            }
+        } catch (Exception e) {
+            logger.info("发生了错误了");
         }
+
     }
 
 
-    /**
-     * 群发自定义消息
-     */
-    public static void sendInfo(MessageEntity message) throws IOException {
-        for (WebSocket item : webSocketSet) {
-            try {
-                item.sendMessage(message);
-            } catch (IOException e) {
-                continue;
+    public void sendMessageTo(String message, String ToUserName) throws IOException {
+        for (WebSocket item : clients.values()) {
+            if (item.username.equals(ToUserName)) {
+                item.session.getAsyncRemote().sendText(message);
+                break;
             }
+        }
+    }
+
+    public void sendMessageAll(String message, String FromUserName) throws IOException {
+        for (WebSocket item : clients.values()) {
+            item.session.getAsyncRemote().sendText(message);
         }
     }
 
     public static synchronized int getOnlineCount() {
-        return onlineCount;
+        return onlineNumber;
     }
 
-    public static synchronized void addOnlineCount() {
-        WebSocket.onlineCount++;
-    }
-
-    public static synchronized void subOnlineCount() {
-        WebSocket.onlineCount--;
-    }
 }
