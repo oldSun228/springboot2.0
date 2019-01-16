196d2424963ed5c348215c009d2aacbfdaafcae7
fan <652810758@qq.com>
2019-01-16 19:16:17 +0800

    websocket代码更新

---
+++ src/main/java/com/xch/study/biz/demo1/controller/Demo4Controller.java
@@ -0,0 +1,23 @@
+package com.xch.study.biz.demo1.controller;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+
+/**
+ * @author fgs
+ * @data 2019/1/8 9:13
+ */
+public class Demo4Controller {
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
