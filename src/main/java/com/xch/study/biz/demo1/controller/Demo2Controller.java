ca43a7b16b4ca753dee8f02929f032d6dccb51ae
fan <652810758@qq.com>
2019-01-16 19:16:17 +0800

    websocket代码更新

---
+++ src/main/java/com/xch/study/biz/demo1/controller/Demo2Controller.java
@@ -0,0 +1,35 @@
+package com.xch.study.biz.demo1.controller;
+
+import org.dom4j.Document;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+
+/**
+ * @author fgs
+ * @data 2019/1/7 17:29
+ */
+public class Demo2Controller {
+    public static void main(String[] args) {
+        String xmlFileName = "C:\\Users\\fan\\Desktop\\JZCX.xml";
+        //读xml
+        String xmlString = Demo2Controller.xmlToString(xmlFileName);
+        System.out.println(xmlString);
+
+    }
+
+    public static String xmlToString(String xmlFileName) {
+        SAXReader saxReader = new SAXReader();
+        Document document;
+        String xmlString = "";
+        try {
+            document = saxReader.read(new File(xmlFileName));
+            xmlString = document.asXML();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            xmlString = "";
+        }
+        return xmlString;
+    }
+}
